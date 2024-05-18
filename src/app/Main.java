0463856535fc693bddb67871c9a4bed741265d27
Johan <johanstrr@gmail.com>
2024-05-18 11:20:36 -0300

    menu print

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package src.app;
+import src.view.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Menu menu = new Menu();
+    }
+}
---
+++ src/view/Menu.java
@@ -0,0 +1,28 @@
+package src.view;
+
+public class Menu {
+   public void showMenu(){
+        System.out.println("Add [1]");
+        System.out.println("List [2]");
+        System.out.println("Daily report [3]");
+        System.out.println("logout [4]");
+   } 
+   public void showAddMenu(){
+        System.out.println("Add admin [1]");
+        System.out.println("Add salesman [2]");
+        System.out.println("Add customer [3]");
+        System.out.println("Add supplier [4]");
+        System.out.println("Add product [5]");
+        System.out.println("Add sale [6]");
+        System.out.println("Exit [7]");
+   }
+   public void showListMenu(){
+        System.out.println("List all salesmen [1]");
+        System.out.println("List all customers [2]");
+        System.out.println("List all suppliers [3]");
+        System.out.println("List all products [4]");
+        System.out.println("List all sales [5]");
+        System.out.println("Exit [6]");
+        
+   }
+}
