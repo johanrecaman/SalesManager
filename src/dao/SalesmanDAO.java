21c4aff8d81a69e71a3f8a7a54384b24a7f37153
Nic <nicpg.7@gmail.com>
2024-06-09 15:35:37 -0300

    get salesman

--- src/dao/SalesmanDAO.java
+++ src/dao/SalesmanDAO.java
@@ -3,12 +3,16 @@ package src.dao;
 import src.config.Database;
 import src.models.Salesman;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SalesmanDAO {
     private Connection connection = Database.getConnection();
     private final String ADD_SALESMAN_SQL = "INSERT INTO Salesman (name, lastname, email, password, birth_date, phone, cpf, city, state, country, address, registration_date) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    private final String READ_SALESMAN_SQL = "SELECT * FROM Salesman";
 
     public void addSalesman(Salesman salesman){
         try {
@@ -31,4 +35,35 @@ public class SalesmanDAO {
             e.printStackTrace();
         }
     }
+
+    public List<Salesman> getSalesman (){
+        List<Salesman> salesmans = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(READ_SALESMAN_SQL);
+            ResultSet result = preparedStatement.executeQuery();
+
+            while(result.next()){
+                Salesman salesman = new Salesman(
+                    result.getInt("id"),
+                    result.getString("name"),
+                    result.getString("lastname"),
+                    result.getString("email"),
+                    result.getString("password"),
+                    result.getDate("birth_date").toLocalDate(),
+                    result.getString("phone"),
+                    result.getString("cpf"),
+                    result.getString("city"),
+                    result.getString("state"),
+                    result.getString("country"),
+                    result.getString("address"),
+                    result.getDate("registration_date").toLocalDate()
+                );
+                salesmans.add(salesman);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
+            return salesmans;
+        }
+
 }
